7e8115fe2edf1f1d6130145ae439f357b8c03ca0
rajeshsahu09 <rajeshsahuise@gmail.com>
2020-09-03 15:48:55 +0530

    create user activity intent added

---
+++ app/src/main/java/com/example/smackapp/CreateUserActivity.kt
@@ -0,0 +1,24 @@
+package com.example.smackapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+
+class CreateUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun onClickAvatarImg(view: View) {
+
+    }
+
+    fun onClickGenerateBckColor(view: View) {
+    }
+
+    fun onClickCreateUser(view: View) {
+
+    }
+}
--- app/src/main/java/com/example/smackapp/LoginActivity.kt
+++ app/src/main/java/com/example/smackapp/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smackapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,12 +9,14 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
 
-        fun onLoginBtnClcik(view: View) {
+    fun onLoginBtnClcik(view: View) {
 
-        }
-        fun onSignupBtnClick(view: View) {
+    }
 
-        }
+    fun onSignupBtnClick(view: View) {
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 }
