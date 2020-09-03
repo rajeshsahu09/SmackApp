eec6fb1bd0dc3be0a74c8de7a8c5bf865c5dc895
rajeshsahu09 <rajeshsahuise@gmail.com>
2020-09-03 15:15:38 +0530

    login/create new activity added

--- app/src/main/java/com/example/smackapp/MainActivity.kt
+++ app/src/main/java/com/example/smackapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smackapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
@@ -30,7 +31,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onLoginClickNavHeader(view: View) {
-
+        val loginIntent = Intent(this, LoginActivity::class.java) // explicit intent
+        startActivity((loginIntent))
     }
 
     fun onChannelBtnNavHeader(view: View) {
