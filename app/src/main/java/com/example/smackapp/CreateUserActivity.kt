96e4a6facd9f579b597aac46e631132b6cfe0ba7
rajeshsahu09 <rajeshsahuise@gmail.com>
2020-09-03 16:12:53 +0530

    Initial Commit

--- app/src/main/java/com/example/smackapp/CreateUserActivity.kt
+++ app/src/main/java/com/example/smackapp/CreateUserActivity.kt
@@ -1,21 +1,51 @@
 package com.example.smackapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_create_user.*
+import kotlin.random.Random
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profiledefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
 
     fun onClickAvatarImg(view: View) {
+        // creates user pic for new users
+        val random = java.util.Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
 
+        if(color == 0) {
+            userAvatar = "light$avatar"
+        } else {
+            userAvatar = "dark$avatar"
+        }
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createuser_avatarimg.setImageResource(resourceId)
     }
 
     fun onClickGenerateBckColor(view: View) {
+        val random = java.util.Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createuser_avatarimg.setBackgroundColor(Color.rgb(r, g, b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
+        println(avatarColor)
     }
 
     fun onClickCreateUser(view: View) {
